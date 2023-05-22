696e5c5b025f07fc00e0d167e69683472460aec2
emrekcgll <emrekcgll@gmail.com>
2023-05-22 13:08:26 +0300

    -

---
+++ WebUI/ViewComponents/Default/CategoryListComponent.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.Default
+{
+    public class CategoryListComponent:ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryListComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _categoryService.TGetList();
+            return View(values);
+        }
+    }
+}
